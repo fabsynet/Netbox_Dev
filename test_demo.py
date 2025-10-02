17e807708b20b5673eb53cad6690061176973d75
fabsynet <146763922+fabsynet@users.noreply.github.com>
2025-10-02 14:51:05 -0600

    Update test_demo.py

--- test_demo.py
+++ test_demo.py
@@ -1,1 +1,9 @@
+from extras.scripts import *
+from django.utils.text import slugify
+from django.contrib.contenttypes.models import ContentType
+from typing import Tuple
 
+from dcim.choices import DeviceStatusChoices
+from dcim.models import Device, DeviceRole, DeviceType, Site, Platform, Interface, Manufacturer, VirtualChassis
+from ipam.models import IPAddress, VLAN, VLANGroup 
+from extras.models import ConfigTemplate
