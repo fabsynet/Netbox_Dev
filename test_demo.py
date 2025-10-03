9c7218a8e7306cca5e0ecc1072436b3f933bdaa2
fabsynet <146763922+fabsynet@users.noreply.github.com>
2025-10-03 11:46:21 -0600

    Update test_demo.py

--- test_demo.py
+++ test_demo.py
@@ -8,7 +8,50 @@ from dcim.models import Device, DeviceRole, DeviceType, Site, Platform, Interfac
 from ipam.models import IPAddress, VLAN, VLANGroup 
 from extras.models import ConfigTemplate
 
+CHOICES = (
+    ('TenGigabitEthernet1/1/1', 'Te1/1/1'),
+    ('TenGigabitEthernet1/1/2', 'Te1/1/2'),
+    ('TenGigabitEthernet1/1/3', 'Te1/1/3'),
+    ('TenGigabitEthernet1/1/4', 'Te1/1/4'),
+    ('TwentyFiveGigabitEthernet1/1/1', 'Twe1/1/1'),
+    ('TwentyFiveGigabitEthernet1/1/2', 'Twe1/1/2'),
+    ('GigabitEthernet1/1', 'Gi1/1'),
+    ('GigabitEthernet1/2', 'Gi1/2'),
+)
 
+LAG_CHOICES = (
+    ('Po1', 'Po1'),
+    ('Po2', 'Po2'),
+    ('Po3', 'Po3'),
+)
+
+choices1 = (
+    ('TenGigabitEthernet1/1/1', 'Te1/1/1'),
+    ('TenGigabitEthernet1/1/2', 'Te1/1/2'),
+    ('TenGigabitEthernet1/1/3', 'Te1/1/3'),
+    ('TenGigabitEthernet1/1/4', 'Te1/1/4'),
+)
+
+choices2 = (
+    ('GigabitEthernet1/1/1', 'Gi1/1/1'),
+    ('GigabitEthernet1/1/2', 'Gi1/1/2'),
+    ('TenGigabitEthernet1/1/3', 'Te1/1/3'),
+    ('TenGigabitEthernet1/1/4', 'Te1/1/4'),
+)
+
+choices3 = (
+    ('TwentyFiveGigabitEthernet1/1/1', 'Twe1/1/1'),
+    ('TwentyFiveGigabitEthernet1/1/2', 'Twe1/1/2'),
+    ('TwentyFiveGigabitEthernet1/1/3', 'Twe1/1/3'),
+    ('TwentyFiveGigabitEthernet1/1/4', 'Twe1/1/4'),
+)
+
+choices4 = (
+    ('GigabitEthernet1/1', 'Gi1/1'),
+    ('GigabitEthernet1/2', 'Gi1/2'),
+    ('GigabitEthernet1/3', 'Gi1/3'),
+    ('GigabitEthernet1/4', 'Gi1/4'),
+)
 
 class DeviceOnboarding(Script):
 
